f2699edc112ec4dc5c7bd47cf9e1bf40059e926f
Reece Pritchard <rapritchard96@gmail.com>
2024-08-14 17:29:50 +0100

    feat: add cell class

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point
+from cell import Cell
 
 def main():
-    start_point = Point(10, 10)
-    end_point = Point(50, 10)
-    line = Line(start_point, end_point)
     win = Window(800, 600)
-    win.draw_line(line)
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     win.wait_for_close()
     
 if __name__ == "__main__":
